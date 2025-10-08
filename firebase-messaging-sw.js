f4006f40db5d7a8bc363c34560431edd74871113
lumixartificial <lumixartificial@gmail.com>
2025-10-08 00:20:33 -0300

    Agrega service worker para notificaciones push 2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-// Versión 4 - Forzar actualización de caché
+// Versión 5 - Forzar actualización de caché
 // Importar los scripts de Firebase necesarios
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
