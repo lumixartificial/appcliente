4b2d7c87d203714649721757c8479916c1743c88
lumixartificial <lumixartificial@gmail.com>
2025-10-13 01:32:50 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,6 @@
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
 
-// Copia tu configuración de Firebase aquí
 const firebaseConfig = {
     apiKey: "AIzaSyBRxJjpH6PBi-GRxOXS8klv-8v91sO4X-Y",
     authDomain: "lumix-financas-app.firebaseapp.com",
@@ -31,32 +30,21 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-
-// Este manejador SÓLO se ejecuta cuando el usuario hace clic en la notificación.
 self.addEventListener('notificationclick', (event) => {
     console.log(`[SW ${SW_VERSION}] Evento 'notificationclick' DETECTADO.`);
     event.notification.close();
-
-    const targetUrl = event.notification.data.url;
+    const targetUrl = event.notification.data.url || self.location.origin;
     
-    if (!targetUrl) {
-        console.error(`[SW ${SW_VERSION}] No se encontró URL en los datos de la notificación. Abriendo página principal.`);
-        return event.waitUntil(clients.openWindow(self.location.origin));
-    }
-
     const promiseChain = clients.matchAll({ type: 'window', includeUncontrolled: true })
     .then((windowClients) => {
         for (const client of windowClients) {
             if (new URL(client.url).origin === new URL(targetUrl).origin && 'focus' in client) {
-                console.log(`[SW ${SW_VERSION}] Ventana encontrada. Navegando y enfocando: ${targetUrl}`);
                 return client.navigate(targetUrl).then(c => c.focus());
             }
         }
         if (clients.openWindow) {
-            console.log(`[SW ${SW_VERSION}] Abriendo nueva ventana en: ${targetUrl}`);
             return clients.openWindow(targetUrl);
         }
     });
-
     event.waitUntil(promiseChain);
 });
